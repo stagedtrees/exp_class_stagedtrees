22b32e4a84f9e10a5c45dac36e38bf40c0a5fb39
gherardovarando <gherardo.varando@gmail.com>
2020-06-04 09:12:18 +0200

    initial

---
+++ install_load_packages.R
@@ -0,0 +1,73 @@
+#############################  PACKAGES  #############################  
+
+# graphical models packages
+install.packages("bnlearn")
+install.packages("devtools")
+devtools::install_github("gherardovarando/stagedtrees", ref = "master")
+
+# Paper "Do we need hundreds of classifiers to solve real world classification problems?" R packages.
+install.packages("nnet")
+install.packages("randomForest")
+install.packages("rpart")
+install.packages("MASS")
+install.packages("rrlda")
+install.packages("sda")
+install.packages("sparseLDA")
+install.packages("mda")
+install.packages("klaR")
+install.packages("HDclassif")
+install.packages("class")
+install.packages("kohonen")
+install.packages("adabag")
+install.packages("caTools")
+install.packages("ipred")
+install.packages("caret")
+install.packages("glmnet")
+install.packages("spls")
+install.packages("pls")
+
+# other packages
+install.packages("e1071")
+install.packages("gam")
+install.packages("polspline")
+install.packages("tree")
+install.packages("ada")
+install.packages("pROC")
+install.packages("InformationValue")
+install.packages("readxl")
+
+
+
+#######   load the libraries
+library(bnlearn)
+library(stagedtrees)
+
+library(nnet)
+library(randomForest)
+library(rpart)
+library(MASS)
+library(rrlda)
+library(sda)
+library(sparseLDA)
+library(mda)
+library(klaR)
+library(HDclassif)
+library(class)
+library(kohonen)
+library(adabag)
+library(caTools)
+library(ipred)
+library(caret)
+library(glmnet)
+library(spls)
+library(pls)
+
+library(e1071)
+library(gam)
+library(polspline)
+library(tree)
+library(ada)
+library(pROC)
+library(InformationValue)
+library(readxl)
+
