8c4de31585dc9d1281565e7f91a8d8c6890aeeb1
FedericoCarli <federico.carli@yahoo.com>
2020-07-13 12:32:44 +0200

    svm algorithm

---
+++ methods/svm_methods.R
@@ -0,0 +1,23 @@
+library(e1071)
+
+predict_svm <- function(model, train, test, optimizecutoff){
+  if (optimizecutoff){
+    prob <- attr(predict(model, newdata = train, probability = T), "probabilities")
+    prob <- prob[, attr(prob, "dimnames")[[2]] == levels(train$answer)[2]]
+    cutoff <- InformationValue::optimalCutoff(actuals = as.numeric(train$answer) - 1, 
+                                              predictedScores = prob, 
+                                              optimiseFor = "Both")
+    prob <- attr(predict(model, newdata = test, probability = T), "probabilities")
+    prob <- prob[, attr(prob, "dimnames")[[2]] == levels(train$answer)[2]]
+    factor(ifelse(prob >= cutoff, levels(train$answer)[2], 
+                  levels(train$answer)[1]), levels = levels(train$answer))
+  }else{
+    predict(model, newdata = test, probability = F)
+  }
+}
+
+
+svm_basic <- function(train, test, optimizecutoff = FALSE, ...) {
+  model <- e1071::svm(answer ~ ., data = train, probability = TRUE)
+  predict_svm(model, train, test, optimizecutoff)
+}
