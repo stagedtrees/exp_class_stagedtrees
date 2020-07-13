fa27fcf630b26834b68392a3e6d05a8e7e53ae0f
FedericoCarli <federico.carli@yahoo.com>
2020-07-13 13:48:33 +0200

    update classifiers object

--- aggregate.R
+++ aggregate.R
@@ -2,35 +2,51 @@ datasets <- read.table("binary_datasets_names.tsv")[,1]
 classifiers <- c(
   "st_full",
   "st_indep",
+  "st_hc_full",
   "st_hc_indep",
   "st_fbhc",
-  "st_bhc",
-  ## st_methods (stagedtrees)
-  "st_bj_kl",
-  "st_bj_tv",
-  "st_bj_cd",
+  "st_fbhc_mi",
+  "st_fbhc_cmi",
+  "st_fbhc_ch",
+  "st_bhc_mi",
+  "st_bhc_cmi",
+  "st_bj_kl_mi",
+  "st_bj_tv_mi",
+  "st_bj_cd_mi",
   "st_naive",
-  ### st_methods  (stagedtrees)
+  "st_naive_mi",
+  "st_naive_cmi",
+  "st_naive_ch",
   "bn_tabu",
-  ## bn_methods (bnlearn)
+  "bn_hc",
   "bnc_nb",
   "bnc_tan_cl",
   "bnc_tan_hc",
   "bnc_fssj",
   "bnc_bsej",
-  ## bnc_methods (bnclassify)
   "bnc_3db",
-  ## nnet_methods (nnet)
-  "nnet_basic",
-  ## glm_methods (glm)
+  "nnet_1",
+  "nnet_2",
+  "rf_1",
+  "rf_2",
   "glm_binomial",
-  ## rf_methods (randomForest)
-  "rf_basic",
-  "simple" ## simple
+  "logistic_basic",
+  "naive_bayes_1",
+  "naive_bayes_2",
+  "cl_tree_1",
+  "cl_tree_2",
+  "regularized_da",
+  "boosting_basic",
+  "bagging_basic",
+  "svm_basic",
+  "gam_basic",
+  "adaboost_basic",
+  "simple"
 )
 
 source("statistics.R")
-nreps <- 20
+k <- 10
+nreps <- 10
 
 TABLE <- array(
   data = NA,
