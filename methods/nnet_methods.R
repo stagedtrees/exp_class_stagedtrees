bcfe5b07199f13e7ad170348f87f2eb9d2526f1d
FedericoCarli <federico.carli@yahoo.com>
2020-07-13 12:35:46 +0200

    two different neural networks

--- methods/nnet_methods.R
+++ methods/nnet_methods.R
@@ -11,13 +11,23 @@ predict_nnet <- function(model, train, test, optimizecutoff){
     factor(ifelse(prob >= cutoff, levels(train$answer)[2], 
                   levels(train$answer)[1]), levels = levels(train$answer))
   }else{
-    predict(model, newdata = test, type = "class")
+    factor(predict(model, newdata = test, type = "class"), levels = levels(train$answer))
   }
 }
 
 
-nnet_basic <- function(train, test, optimizecutoff = FALSE, ...){
+nnet_1 <- function(train, test, optimizecutoff = FALSE, ...){
   model <- nnet(answer ~ ., data = train, size = 20, 
        decay = 0.01, maxit = 5000, linout = FALSE, MaxNWts = 5000, trace = FALSE)
   predict_nnet(model, train, test, optimizecutoff)
 }
+
+nnet_2 <- function(train, test, optimizecutoff = FALSE, ...){
+  model <- nnet(answer ~ ., data = train, size = 10, 
+                decay = 0.001, maxit = 5000, linout = FALSE, MaxNWts = 5000, trace = FALSE)
+  predict_nnet(model, train, test, optimizecutoff)
+}
+
+
+
+
