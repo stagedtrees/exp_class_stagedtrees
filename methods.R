d954ee11c560311183d56a93e29e6f36c67a023a
FedericoCarli <federico.carli@yahoo.com>
2020-07-13 13:49:36 +0200

    update classifiers object and list of source of methods to upload

--- methods.R
+++ methods.R
@@ -1,34 +1,66 @@
 ### all classifers are a function of train and test (plu additional args)
 ### and return predictions
 
-
 source("methods/st_methods.R")
 source("methods/bn_methods.R")
 source("methods/bnc_methods.R")
 source("methods/nnet_methods.R")
 source("methods/glm_methods.R")
 source("methods/rf_methods.R")
+source("methods/logistic_methods.R")
+source("methods/tree_methods.R")
+source("methods/discriminant_analysis_methods.R")
+source("methods/naive_methods.R")
+source("methods/naive_2_methods.R")
+source("methods/boosting_methods.R")
+source("methods/bagging_methods.R")
+source("methods/adaboost_methods.R")
+source("methods/svm_methods.R")
+source("methods/gam_methods.R")
 
 classifiers <- c(
-  ## st_methods (stagedtrees) 
-  "st_full", "st_indep",  
-  #"st_hc_indep",
-  "st_fbhc", 
-  "st_bhc", 
-  "st_bj_kl", "st_bj_tv", "st_bj_cd", 
+  "st_full",
+  "st_indep",
+  "st_hc_full",
+  "st_hc_indep",
+  "st_fbhc",
+  "st_fbhc_mi",
+  "st_fbhc_cmi",
+  "st_fbhc_ch",
+  "st_bhc_mi",
+  "st_bhc_cmi",
+  "st_bj_kl_mi",
+  "st_bj_tv_mi",
+  "st_bj_cd_mi",
   "st_naive",
-  ## bn_methods (bnlearn)
-  "bn_tabu", 
-  ## bnc_methods (bnclassify)
-  "bnc_nb", "bnc_tan_cl", "bnc_tan_hc", "bnc_fssj", "bnc_bsej",  
-  "bnc_3db", 
-  ## nnet_methods (nnet)
-  "nnet_basic",
-  ## glm_methods (glm)
+  "st_naive_mi",
+  "st_naive_cmi",
+  "st_naive_ch",
+  "bn_tabu",
+  "bn_hc",
+  "bnc_nb",
+  "bnc_tan_cl",
+  "bnc_tan_hc",
+  "bnc_fssj",
+  "bnc_bsej",
+  "bnc_3db",
+  "nnet_1",
+  "nnet_2",
+  "rf_1",
+  "rf_2",
   "glm_binomial",
-  ## rf_methods (randomForest)
-  "rf+basic",
-  "simple" ## simple
+  "logistic_basic",
+  "naive_bayes_1",
+  "naive_bayes_2",
+  "cl_tree_1",
+  "cl_tree_2",
+  "regularized_da",
+  "boosting_basic",
+  "bagging_basic",
+  "svm_basic",
+  "gam_basic",
+  "adaboost_basic",
+  "simple"
 )
 
 
