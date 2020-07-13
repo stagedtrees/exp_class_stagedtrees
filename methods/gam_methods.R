e3bd4927bc50e13c543a0129877906f537a85bc6
FedericoCarli <federico.carli@yahoo.com>
2020-07-13 12:32:58 +0200

    gam algorithm

---
+++ methods/gam_methods.R
@@ -0,0 +1,24 @@
+library(gam)
+
+predict_gam <- function(model, train, test, optimizecutoff){
+  if (optimizecutoff){
+    prob <- predict(model, newdata = train, type = "response")
+    cutoff <- InformationValue::optimalCutoff(actuals = as.numeric(train$answer) - 1, 
+                                              predictedScores = prob, 
+                                              optimiseFor = "Both")
+    prob <- predict(model, newdata = test, type = "response")
+    factor(ifelse(prob >= cutoff, levels(train$answer)[2], 
+                  levels(train$answer)[1]), levels = levels(train$answer))
+  }else{
+    cutoff <- 0.5
+  }
+  prob <- predict(model, newdata = test, type = "response")
+  factor(ifelse(prob >= cutoff, levels(train$answer)[2], 
+                levels(train$answer)[1]), levels = levels(train$answer))
+}
+
+
+gam_basic <- function(train, test, optimizecutoff = FALSE, ...) {
+  suppressWarnings(model <- gam(answer ~ ., data = train, family = binomial))
+  predict_gam(model, train, test, optimizecutoff)
+}
