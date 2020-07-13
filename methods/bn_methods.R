441bf834bd2b9fd286e088a4f82589bd99a6b6fc
FedericoCarli <federico.carli@yahoo.com>
2020-07-13 12:35:11 +0200

    bnlearn models

--- methods/bn_methods.R
+++ methods/bn_methods.R
@@ -4,7 +4,7 @@ predict_bn <- function(model, train, test, optimizecutoff){
   if (optimizecutoff){
     prob <- t(attr(predict(model, node = "answer", data = train, prob = TRUE), "prob"))
     attr(prob, "dimnames") <- NULL
-    cutoff <- optimalCutoff(as.numeric(test$answer) - 1, prob[, 2], optimiseFor = "Both")
+    cutoff <- optimalCutoff(as.numeric(train$answer) - 1, prob[, 2], optimiseFor = "Both")
     prob <- t(attr(predict(model, node = "answer", data = test, prob = TRUE), "prob"))
     attr(prob, "dimnames") <- NULL
     factor(ifelse(prob[, 2] >= cutoff, levels(train$answer)[2], 
@@ -24,4 +24,12 @@ bn_tabu <- function(train, test, optimizecutoff = FALSE){
   bn.fit <- bn.fit(bn, train)
   predict_bn(bn.fit, train, test, optimizecutoff)
 }
+
+bn_hc <- function(train, test, optimizecutoff = FALSE) {
+  bn <- bnlearn::hc(train)
+  bn.fit <- bn.fit(bn, train)
+  predict_bn(bn.fit, train, test, optimizecutoff)
+}
+
+
 
